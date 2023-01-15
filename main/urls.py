9db64f73dca323ece64ef940a8ee31660ba458d9
uluk001 <ulukmanmuratov@gmail.com>
2023-01-15 14:21:39 +0600

    add retrive

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,7 @@ from main import views
 
 urlpatterns = [
     path('create/', views.CreateToDoView.as_view()),
+    path('todos/', views.ListTodoView.as_view()),
+    path('retrive/<int:pk>', views.RetrieveTodoView.as_view()),
+    path('destroy', views.DestroyTodoView  .as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from main.models import Todo
+from rest_framework.response import Response
 from main.serializers import CreateToDoSerializer
 
 from rest_framework import generics
@@ -14,3 +15,19 @@ class ListTodoView(generics.ListAPIView):
     queryset = Todo.objects.all()
     serializer_class = CreateToDoSerializer
     
+
+class RetrieveTodoView(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = CreateToDoSerializer
+
+class DestroyTodoView(generics.DestroyAPIView):
+    # queryset = Todo.objects.all()
+    serializer_class = CreateToDoSerializer
+
+    def delete(self, request, *args, **kwargs):
+        todo =Todo.objects.get(id=pk)
+        # i.delete()
+        return Response({'ALL':'DELETED'})
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
